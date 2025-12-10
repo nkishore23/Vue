de362dbfab0384c105ce1d6cef5c73e0501b3337
Kishore N <nkishore2390@gmail.com>
2025-12-10 15:46:51 +0530

    sample page

---
+++ vuedemo/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    };
+  },
+    methods: {
+      increment() {
+        this.count++;
+      },
+      decrement() {
+        this.count--;
+      },
+      reset() {
+        this.count = 0
+      },
+      maxValue() {
+        this.count = 10;
+      }
+    }
+};
+</script>
+
+<template>
+
+
+<h2>{{count}}</h2>
+  <div class="card">
+    <button type="button" @click="increment">Increment</button>
+
+    <button type="button" @click="decrement">Decrement</button>
+
+    <button type="button" @click="reset">Reset</button>
+
+    <button type="button" @click="maxValue">Max Value</button>
+   
+  </div>
+
+  
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
